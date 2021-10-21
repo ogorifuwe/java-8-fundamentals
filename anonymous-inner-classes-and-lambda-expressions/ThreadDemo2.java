73082f153778e1a80770ae5a0dcf51022a656ca6
oi <oifuwe@luc.edu>
2021-10-21 12:34:35 -0500

    add anonymous inner class and lambda expressions

---
+++ anonymous-inner-classes-and-lambda-expressions/ThreadDemo2.java
@@ -0,0 +1,14 @@
+class ThreadDemo2 {
+
+  public static void main(String[] args) {
+    Thread t = new Thread(() -> {
+      for (int i = 0; i < 10; ++i)
+        System.out.println("child thread");
+    });
+
+    t.start();
+
+    for (int i = 0; i < 10; ++i)
+      System.out.println("main thread");
+  }
+}
